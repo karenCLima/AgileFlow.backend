ef5c9207baefeb67853e73420743e42956cd4155
karenCLima <lima.kcls@gmail.com>
2025-01-06 16:50:22 -0300

    feat: creating repositories

---
+++ src/main/java/com/br/AgileFlow/backend/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.br.AgileFlow.backend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.br.AgileFlow.backend.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
